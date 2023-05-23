878108c82479ef4c1dbb86e9d3cc1a3a763d1448
jenya-b <evgeny_rab@inbox.ru>
2023-05-23 15:59:01 +0300

    feat: get data by fetch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,29 +2,45 @@ import { useEffect, useState } from 'react';
 
 import { Header } from 'components/Header/Header';
 import { Main } from 'components/Main/Main';
-import { data } from 'data';
 import './App.scss';
 
 export const App = () => {
+  const [data, setData] = useState();
   const [tabNames, setTabNames] = useState([]);
   const [valueTab, setValueTab] = useState('');
 
   useEffect(() => {
-    if (!data || !data.data) return;
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data.json');
+        const json = await response.json();
+        setData(json.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    const names = Object.keys(data.data);
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const names = Object.keys(data);
     setValueTab(names[0]);
     setTabNames(names);
-  }, []);
+  }, [data]);
 
   const writeTabValue = (event, newValue) => {
     setValueTab(newValue);
   };
 
+  if (!data) return <h3>Нет данных для отображения</h3>;
+
   return (
     <div className="wrapper">
       <Header tabNames={tabNames} valueTab={valueTab} writeTabValue={writeTabValue} />
-      <Main data={data.data[valueTab]} />
+      <Main data={data[valueTab]} />
     </div>
   );
 };
