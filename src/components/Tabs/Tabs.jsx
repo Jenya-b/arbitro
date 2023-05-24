5b38eea763d058b052d3ab30c8599ae619fa0409
jenya-b <evgeny_rab@inbox.ru>
2023-05-24 12:14:22 +0300

    feat: format menu text

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,10 +1,11 @@
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { formatMenuText } from 'utils/formatText';
 
 export const BasicTabs = ({ handleChange = (f) => f, value, tabNames }) => (
   <Tabs value={value} onChange={handleChange}>
     {tabNames.map((item, index) => (
-      <Tab key={index} value={item} label={item} />
+      <Tab key={index} value={item} label={formatMenuText(item)} />
     ))}
   </Tabs>
 );
---
+++ src/utils/formatText.js
@@ -0,0 +1,1 @@
+export const formatMenuText = (str) => str.replace('_', '-');
